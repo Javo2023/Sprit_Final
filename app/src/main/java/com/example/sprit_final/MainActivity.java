44edfd0b408bec6aa5cd5f3935895aef26cb6934
javo2023 <jguzmanalarcon@gmail.com>
2023-06-29 22:21:43 -0400

    cuarto commit ok whatsapp y linkedin falta correo

--- app/src/main/java/com/example/sprit_final/MainActivity.java
+++ app/src/main/java/com/example/sprit_final/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.sprit_final;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.sprit_final.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.secondActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent secondActivity =  new Intent(getBaseContext(),SecondActivity.class);
+                startActivity(secondActivity);
+            }
+        });
+
+
+
+
     }
+
+
 }
--- app/src/main/java/com/example/sprit_final/SecondActivity.java
+++ app/src/main/java/com/example/sprit_final/SecondActivity.java
@@ -2,13 +2,53 @@ package com.example.sprit_final;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.sprit_final.databinding.ActivitySecondBinding;
 
 public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+
+        ActivitySecondBinding binding = ActivitySecondBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.buttonCorreo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {sendEmail();
+
+
+            }
+            private void sendEmail() {
+            }
+        });
+
+
+        binding.buttonLinkedin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLinkedin = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.linkedin.com/in/mafu8/"));
+                startActivity(intentLinkedin);
+            }
+        });
+
+        binding.buttonWhatsapp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentWhatsapp = new Intent(Intent.ACTION_VIEW,Uri.parse("https://api.whatsapp.com/send?phone=+56992401294"));
+                startActivity(intentWhatsapp);
+            }
+        });
+
+
+
     }
+
+
 }
